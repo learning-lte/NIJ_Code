560fed8d29a667f8dc0bced17e9bbb13151f3ad2
nicksmith37 <ngsmith37@gmail.com>
2020-05-21 15:02:53 -0500

    Adding Base Code for WiFi Data Collection that includes ability to collect and sort physical WiFi data, collect from different frequencies, and have user-specified buffers at beginning and end

---
+++ WifiDataOrganization.py
@@ -0,0 +1,199 @@
+import imp
+import scipy
+import numpy
+import sys
+import uuid
+import matplotlib.pyplot as plt
+#from gnuradio.blocks import parse_file_metadata
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+def autoCorMatch(frame):
+    autoCorList = []
+
+    if (len(frame) > 15000):
+
+        # Calculate first sample's autoCorrelation and alter to get rest
+        current_auto = 0
+        current_power = 0
+        for index2 in range(48 + 15):
+            current_auto += frame[index2] * numpy.conj(frame[index2 + 16])
+        for index3 in range(48 - 1):
+            current_power += frame[index3] * numpy.conj(frame[index3])
+
+        autoSample = abs(current_auto) / current_power
+        autoAverage = autoSample
+        autoMax = autoSample
+
+        # Alter with autoCorrelate function
+        for sampIndex in range(len(frame)):
+            if (sampIndex == len(frame) - 64 - 16):
+                break
+            current_auto -= (frame[sampIndex] * numpy.conj(frame[sampIndex + 16]))
+            current_power -= frame[sampIndex] * numpy.conj(frame[sampIndex])
+            current_auto += (frame[sampIndex + 64] * numpy.conj(frame[sampIndex + 64 + 16]))
+            current_power += frame[sampIndex + 48] * numpy.conj(frame[sampIndex + 48])
+            autoSample = (abs(current_auto) / current_power)
+            autoCorList.append(autoSample)
+            if (autoSample > autoMax):
+                autoMax = autoSample
+            if (autoSample < 0):
+                return None
+            autoAverage += autoSample
+        autoAverage = autoAverage / len(autoCorList)
+        if autoMax - autoAverage > 1 and autoMax > 50:
+            return autoCorList
+
+    return None
+
+
+def main():
+    fileName = ""
+    sys.path.insert(0, '/home/nij/GNU/main/lib/python2.7/dist-packages/')
+    sys.path.insert(0, '/home/nij/GNU/main/lib/python2.7/dist-packages/pmt/')
+    sys.path.insert(0, '/home/nij/GNU/main/lib/')
+    parseHeader = imp.load_source('gr_read_file_metadata_nick', '/home/nij/GNU/main/bin/gr_read_file_metadata_nick')
+
+    base_path = "/home/nij/GNU/"
+    raw_data = scipy.fromfile(open(base_path + "file_sink" + fileName), dtype=scipy.complex64)
+    parsed_data = open(base_path + "parsed_data" + fileName + ".txt", "r")
+    finished_data = open("finished_data" + fileName + ".txt", "w")
+    raw_frame = []
+    indiv_frame = []
+    total_frames = []
+    # Returns a 2D array containing raw data header info sorted by frame
+    # Filters combined data for NULL frames
+    # All units are bytes
+    # Format: HEADER LENGTH, SIZE IN Items, Frame Counter
+
+    header_data = parseHeader.main(base_path + "file_sink" + fileName + ".hdr", True)
+    header_data = list(filter(None, header_data))
+
+    # Sort raw data into frames based on header info
+    # Combines parsed data, raw data and assigns frames ID if the raw frame wasnt skipped
+    # Parsed Data Format: Transmitter MAC, Frame Number (Counter)
+    # Final Format: MAC ADDRESS, FRAME ID, FRAME LENGTH (in items), [RAW DATA]
+    index = 0
+    last_raw_counter = 0
+    last_frame_number = 0
+    start_num = 0
+    freq_list = []
+
+    for header in header_data:
+
+        # If not at end of parsed_data file, reads in next line
+        # Frame_number is next frame recorded. If raw_frame is not sequentially in that place, it is skipped
+        last_pos = parsed_data.tell()
+        current_line = parsed_data.read()
+        raw_frame_counter = int(header[2])
+
+        if (header[1] != 0):
+            if (current_line != ""):
+                parsed_data.seek(last_pos)
+                mac_addr, frame_number = parsed_data.readline().split(",")
+                frame_number = int(frame_number.strip("\n"))
+
+                # This while loop gets rid of duplicate frame numbers in parsed data by
+                # repeatedly reading lines until the next frame number is not equal to the last saved
+                while (last_frame_number == frame_number):
+                    mac_addr, frame_number = parsed_data.readline().split(",")
+                    frame_number = int(frame_number.strip("\n"))
+                    last_pos = parsed_data.tell()
+
+            """Frame Number is the number associated with the frame in the parsed data text file. Raw Frame counter is the number
+            saved in the metadata file associated with that raw frame. Both should always be increasing (although Frame Number can
+            have duplicates). This if statement checks to see:
+                1. Data was saved correctly in the raw binary file
+                2. if frame number is less than raw_frame_counter (Should never happen), then skip the raw_frame and that frame number
+                3. if frame number is greater than raw_frame_counter (most common), then keep frame number and skip raw_frame
+                4. Skip 1st 100 frames captured correctly and if any frames come from zeroed MAC addresses (potential virtual machines or spoofs)
+                5. Proceed with saving successfully captured frame"""
+
+            if (raw_frame_counter != (last_raw_counter + 1)):
+                raise Exception("Error in raw data of collected frames at ", raw_frame_counter)
+                pass
+            elif (frame_number < raw_frame_counter):
+                index += header[1]
+            elif (frame_number > raw_frame_counter):
+                parsed_data.seek(last_pos)
+                index += header[1]
+            elif (start_num == 100 or mac_addr == "00:0:0:0:0:0"):
+                start_num += 1
+                index += header[1]
+            else:
+                # Reads in raw frame and updates last frame number that has been read (used to remove duplicates above)
+                last_frame_number = frame_number
+                for each_item in range(header[1]):
+                    raw_frame.append(raw_data[index + each_item])
+                # Calls autocorrelation function and increments index for raw binary file
+                test1 = autoCorMatch(raw_frame)
+                index += header[1]
+                # Used to ensure there is not more than 100 frames captured from each device
+                flag1 = False
+                flag2 = False
+                num_save = 149
+                if (test1 is not None):
+                    for addr in freq_list:
+                        if mac_addr == addr[0]:
+                            if addr[1] <= num_save:
+                                addr[1] += 1
+                                flag2 = True
+                            flag1 = True
+                    if not flag1:
+                        freq_list.append([mac_addr, 1])
+
+                # If autocorrelation function returned something (Success!) then save the MAC addr,
+                # globally unique identifier, raw data length, and raw data to array to be written to file
+                ####test1 is not None and
+                if (test1 is not None and flag2):
+                    indiv_frame.append(mac_addr)
+                    indiv_frame.append(frame_number)
+                    # indiv_frame.append(str(uuid.uuid4()))
+                    indiv_frame.append(header[1])
+                    indiv_frame.append(raw_frame)
+                    total_frames.append(indiv_frame)
+                    ###Used for testing
+                    # test_plots(raw_frame, test1)
+
+        last_raw_counter = raw_frame_counter
+        raw_frame = []
+        indiv_frame = []
+
+    parsed_data.close()
+    if file_len(base_path + "parsed_data" + fileName + ".txt") < len(total_frames):
+        raise Exception("Captured too many frames.")
+
+    # Sorts frames based on MAC Addresses
+    # Writes frames to text file
+    # total_frames = list(filter(None, total_frames))
+    # total_frames.sort()
+
+    for each in total_frames:
+        finished_data.write(
+            str(each[0]) + "|" + str(each[1]) + "|" + str(each[2]) + "|" + str(each[3]).strip("[] ") + "\n")
+
+    print("Done")
+    # Difference should always be less than MAX FRAME SIZE (No Buffer at 20 MHz = 43200)
+    print("Index length:", index, "Actual length:", len(raw_data), "Difference: ", len(raw_data) - index)
+    print(freq_list)
+
+    finished_data.close()
+
+
+def test_plots(raw_frame, test1):
+    fig, ax = plt.subplots(4)
+    fig.suptitle('Autocorrelation, Magnitude, Power, Freq Plots')
+    ax[0].plot(test1, 'y')
+    ax[1].plot(list(map(lambda x: abs(x), raw_frame)), 'b')
+    ax[2].plot(list(map(lambda x: abs(x) ** 2, raw_frame)), 'r')
+    ax[3].plot(abs(numpy.fft.fftshift(numpy.fft.fft(raw_frame))), 'g')
+    plt.show()
+
+
+main()
---
+++ src/gr-ieee-80211/lib/sync_short.cc
@@ -0,0 +1,192 @@
+/*
+ * Copyright (C) 2013, 2016 Bastian Bloessl <bloessl@ccs-labs.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <ieee802-11/sync_short.h>
+#include <gnuradio/io_signature.h>
+#include "utils.h"
+
+#include <iostream>
+
+using namespace gr::ieee802_11;
+
+static const int MIN_GAP = 480;
+static const int MAX_SAMPLES = 540 * 80;
+int frame_counter = 0;
+
+class sync_short_impl : public sync_short {
+
+public:
+sync_short_impl(double threshold, unsigned int min_plateau, unsigned int samp_fact, double start_buffer, double end_buffer, bool log, bool debug) :
+		block("sync_short",
+			gr::io_signature::makev(4, 4, get_input_sizes()),
+			gr::io_signature::make(2, 2, sizeof(gr_complex))),
+		d_log(log),
+		d_debug(debug),
+		d_state(SEARCH),
+		d_plateau(0),
+		d_freq_offset(0),
+		d_copied(0),
+		MIN_PLATEAU(min_plateau),
+		d_threshold(threshold),
+		d_samp_fact(samp_fact),
+		d_start_buffer(start_buffer),
+		d_end_buffer(end_buffer),
+		d_frame_found(false){
+
+	set_tag_propagation_policy(block::TPP_DONT);
+}
+
+int general_work (int noutput_items, gr_vector_int& ninput_items,
+		gr_vector_const_void_star& input_items,
+		gr_vector_void_star& output_items) {
+
+	const gr_complex *in = (const gr_complex*)input_items[0];
+	const gr_complex *in_abs = (const gr_complex*)input_items[1];
+	const float *in_cor = (const float*)input_items[2];
+	const gr_complex *in_delayed = (const gr_complex *)input_items[3];
+	gr_complex *out = (gr_complex*)output_items[0];
+	gr_complex *out1 = (gr_complex*)output_items[1];
+
+	int noutput = noutput_items;
+	int ninput = std::min(std::min(ninput_items[0], ninput_items[1]), ninput_items[2]);
+	
+	//dout << "Sampling Factor: " << d_samp_fact << std::endl;
+	//dout << "SHORT noutput : " << noutput << " Orig ninput: " << ninput_items[0]  << " 40 MHZ ninput: " << ninput_items[3] << std::endl;
+
+	switch(d_state) {
+
+	case SEARCH: {
+		int i;
+		
+		for(i = 0; i < ninput; i++) {
+			if(in_cor[i] > d_threshold) {
+				if(d_plateau < MIN_PLATEAU) {
+					d_plateau++;
+
+				} else {
+					d_state = COPY;
+					d_copied = 0;
+					d_freq_offset = arg(in_abs[i]) / 16;
+					d_plateau = 0;
+					insert_tag(nitems_written(0), d_freq_offset, nitems_read(0) + i, 0);
+					//dout << "SHORT Frame!" << std::endl;
+					break;
+				}
+			} else {
+				d_plateau = 0;
+			}
+		}
+
+		consume_each(i);
+		consume(3, i*(d_samp_fact -1));
+		return 0;
+	}
+
+	case COPY: {
+
+		int o = 0;
+		
+		while( o < ninput && o < noutput && d_copied < (MAX_SAMPLES + d_start_buffer + d_end_buffer)) {
+			if(in_cor[o] > d_threshold) {
+				if(d_plateau < MIN_PLATEAU) {
+					d_plateau++;
+
+				// there's another frame
+				} else if(d_copied > MIN_GAP) {
+					d_copied = 0;
+					d_plateau = 0;
+					d_freq_offset = arg(in_abs[o]) / 16;
+					insert_tag(nitems_written(0) + o, d_freq_offset, nitems_read(0) + o, (d_samp_fact - 1) * o + 1);
+					//dout << "Another SHORT Frame!" << std::endl;
+					break;
+				}
+
+			} else {
+				d_plateau = 0;
+			}
+
+			out[o] = in[o] * exp(gr_complex(0, -d_freq_offset * d_copied));
+			for (int j = 0; j < d_samp_fact; j++){
+				out1[o * d_samp_fact + j] = in_delayed[o * d_samp_fact + j];
+			}
+			o++;
+			d_copied++;
+		}
+
+		if(d_copied == MAX_SAMPLES + d_start_buffer + d_end_buffer) {
+			d_state = SEARCH;
+		}
+
+		//dout << "SHORT copied " << o << " SHORT copied buffer " << (o1 + o*d_samp_fact) << std::endl;
+		//dout << "Number Written 20: " << nitems_written(0) << " Number Written 40: " << nitems_written(1) << std::endl;
+		dout << "Number Read 20: " << nitems_read(0) << " Number Read 40: " << nitems_read(3) << std::endl;
+
+		consume_each(o);
+		consume(3, o*(d_samp_fact-1));
+		
+		produce(0, o);
+		produce(1, o*d_samp_fact);
+		o = 0;
+		return WORK_CALLED_PRODUCE;
+	}
+	}
+
+	throw std::runtime_error("sync short: unknown state");
+	return 0;
+}
+
+
+void insert_tag(uint64_t item, double freq_offset, uint64_t input_item, int o) {
+	mylog(boost::format("frame start at in: %2% out: %1% out40: %3%") % item % input_item % nitems_read(3));
+	frame_counter += 1;
+	const pmt::pmt_t key = pmt::string_to_symbol("wifi_start");
+	const pmt::pmt_t value = pmt::from_double(freq_offset);
+	const pmt::pmt_t srcid = pmt::string_to_symbol(std::to_string(frame_counter));
+	add_item_tag(0, item, key, value, srcid);
+	const pmt::pmt_t value1 = pmt::from_uint64(frame_counter);
+	add_item_tag(1, (item - nitems_written(0) + nitems_written(1) + o * (d_samp_fact - 1)), key, value1, srcid);
+	//dout << "SS:Frame Counter: " << frame_counter << "Normal Stream Position: " << nitems_written(0) << "40MHz Stream Position: " << nitems_written(1)  << ", " << (nitems_written(1) + o) << std::endl;
+}
+
+
+static std::vector<int> get_input_sizes(){
+     return {
+         sizeof(gr_complex),
+         sizeof(gr_complex),
+         sizeof(float),
+         sizeof(gr_complex),
+     };
+}
+
+private:
+	enum {SEARCH, COPY} d_state;
+	int d_copied;
+	int d_plateau;
+	float d_freq_offset;
+	const double d_threshold;
+	const bool d_log;
+	const bool d_debug;
+	const unsigned int MIN_PLATEAU;
+	const  double d_start_buffer;
+	const double d_end_buffer;
+	bool d_frame_found;
+	const unsigned int d_samp_fact;
+};
+
+sync_short::sptr
+sync_short::make(double threshold, unsigned int min_plateau, unsigned int samp_fact, double start_buffer, double end_buffer, bool log, bool debug) {
+	return gnuradio::get_initial_sptr(new sync_short_impl(threshold, min_plateau, samp_fact, start_buffer, end_buffer, log, debug));
+}
